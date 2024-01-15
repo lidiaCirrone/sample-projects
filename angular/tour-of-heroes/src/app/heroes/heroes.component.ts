9a3486a92327203eb1469e56eb5bfacd09a68350
Lidia Cirrone <lidia.cirrone@gmail.com>
2024-01-15 16:22:18 +0100

    heroService observable + mocked HTTP request

--- angular/tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular/tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,16 +1,26 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.scss',
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  constructor(private heroService: HeroService) {}
+
+  heroes: Hero[] = [];
   selectedHero?: Hero;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
   }
 }
