21e274822e1bced278e5be5cae6abee7ae990fb0
Lidia Cirrone <lidia.cirrone@gmail.com>
2024-01-15 18:01:19 +0100

    dashboard component + added routing

--- angular/tour-of-heroes/src/app/app.module.ts
+++ angular/tour-of-heroes/src/app/app.module.ts
@@ -8,20 +8,18 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent,
+    DashboardComponent,
     HeroDetailComponent,
-    MessagesComponent
+    HeroesComponent,
+    MessagesComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
